0c467b0d87f5a191923c25e39d08f34eeb35b3b2
Hlib <gfietisov@gmail.com>
2024-06-05 16:04:11 +0300

    implemented both additional tasks

---
+++ src/main/java/com/example/task_2/Ticket.java
@@ -0,0 +1,61 @@
+package com.example.task_2;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+//There was nothing mentioned about getters + setters, so they are not present
+public class Ticket {
+
+    private String id;
+    private String concertHall;
+    private short eventCode;
+    private long unixTime;
+    private boolean isPromo;
+    private char stadiumSector;
+    private float maxBackpackWeight;
+    private BigDecimal price;
+
+    public Ticket() {
+        setCreationTime();
+    }
+
+    public Ticket(String concertHall, short eventCode) { //as long as there is an automatic time detection, `time` param is omitted in this constructor
+        this.concertHall = concertHall;
+        this.eventCode = eventCode;
+
+        setCreationTime();
+    }
+
+    public Ticket(String id, String concertHall, short eventCode, long unixTime, boolean isPromo, char stadiumSector,
+                  float maxBackpackWeight,BigDecimal price) {
+        this.id = id;
+        this.concertHall = concertHall;
+        this.eventCode = eventCode;
+        this.unixTime = unixTime;
+        this.isPromo = isPromo;
+        this.stadiumSector = stadiumSector;
+        this.maxBackpackWeight = maxBackpackWeight;
+        this.price = price;
+
+        setCreationTime();
+    }
+
+    private void setCreationTime() {
+        this.unixTime = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+    };
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "id='" + id + '\'' +
+                ", concertHall='" + concertHall + '\'' +
+                ", eventCode=" + eventCode +
+                ", unixTime=" + unixTime +
+                ", isPromo=" + isPromo +
+                ", stadiumSector=" + stadiumSector +
+                ", maxBackpackWeight=" + maxBackpackWeight +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/task_2/TicketService.java
@@ -0,0 +1,23 @@
+package com.example.task_2;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class TicketService {
+
+    public static void main(String[] args) {
+        Ticket emptyTicket = new Ticket();
+        System.out.println("Empty ticket: " + emptyTicket);
+
+        Ticket limitedTicket = new Ticket("Great Hall", (short) 101);
+        System.out.println("Limited ticket: " + limitedTicket);
+
+        Ticket fullTicket = new Ticket("A1lP", "The smallest", (short) 299, 1717580801L, false,
+                'B', 10.5f, new BigDecimal("399.99"));
+        System.out.println("Full ticket: " + fullTicket);
+    }
+
+
+}
