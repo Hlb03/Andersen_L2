4dc94969d9620dda7735aeea2452e78e6c846c53
Hlib <gfietisov@gmail.com>
2024-06-06 20:24:28 +0300

    JFB-1:override equals and hashCode in Ticket + add ticket storage in TicketService

--- src/main/java/com/example/task_2/TicketService.java
+++ src/main/java/com/example/task_2/TicketService.java
@@ -2,10 +2,25 @@ package com.example.task_2;
 
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TicketService {
+
+    private final static Map<String, Ticket> ticketStorage = new HashMap<>();
+
+    static {
+        ticketStorage.put("1", new Ticket("Hall", (short) 551));
+        ticketStorage.put("2", new Ticket("Concert hall", (short) 990));
+        ticketStorage.put("3", new Ticket("Opera Hall", (short) 13));
+        ticketStorage.put("4", new Ticket());
+        ticketStorage.put("5", new Ticket());
+        ticketStorage.put("6", new Ticket());
+        ticketStorage.put("7", new Ticket("7", "Some Hall", (short) 885, false, 'A', 13.5f, new BigDecimal("350.50")));
+        ticketStorage.put("8", new Ticket("7", "Another", (short) 10, true, 'A', 13.5f, new BigDecimal("350.50")));
+        ticketStorage.put("9", new Ticket("7", "One more", (short) 192, true, 'C', 21.3f, new BigDecimal("350.50")));
+        ticketStorage.put("10", new Ticket("7", "New one", (short) 301, false, 'B', 4.0f, new BigDecimal("350.50")));
+    }
 
     public static void main(String[] args) {
         Ticket emptyTicket = new Ticket();
@@ -17,6 +32,8 @@ public class TicketService {
         Ticket fullTicket = new Ticket("A1lP", "The smallest", (short) 299, false,
                 'B', 10.5f, new BigDecimal("399.99"));
         System.out.println("Full ticket: " + fullTicket);
+
+        System.out.println(ticketStorage.get("8"));
     }
 
 
