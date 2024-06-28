8ce71633a65a23f3a83bef2243f6af6d17912fee
Hlib <gfietisov@gmail.com>
2024-06-28 12:07:45 +0300

    create BusTicket service with functionality to add, remote and getAll by the certain params

--- src/main/java/com/example/task_2/bus_ticket/BusTicket.java
+++ src/main/java/com/example/task_2/bus_ticket/BusTicket.java
@@ -4,11 +4,19 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Data
 public class BusTicket {
-    String ticketClass;
-    String ticketType;
-    LocalDate startDate;
-    BigDecimal price;
+
+    public BusTicket(String ticketType, LocalDate startDate) {
+        this.ticketType = ticketType;
+        this.startDate = startDate;
+    }
+
+    private final UUID id = UUID.randomUUID();
+    private String ticketClass;
+    private String ticketType;
+    private LocalDate startDate;
+    private BigDecimal price;
 }
